2d35273870d806139c6d25fe3e2ab4c50538034d
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-09-26 11:18:40 -0300

    Exibindo cores na ordem inversa da que foi informada

--- src/set/exercicios_propostos/Ex1.java
+++ src/set/exercicios_propostos/Ex1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Ex1 {
     public static void main(String[] args) {
 
-        Set<String> arcoiris = new HashSet<>(Arrays.asList("Vermelho", "Laranja", "Amarelo", "Verde", "Azul", "Azul-Escuro", "Violeta"));
+        Set<String> arcoiris = new LinkedHashSet<>(Arrays.asList("Vermelho", "Laranja", "Amarelo", "Verde", "Azul", "Azul-Escuro", "Violeta"));
 
         //Exiba todas as cores uma abaixo da outra
         System.out.println("--Todas as cores do Arco-Íris--");
@@ -14,14 +14,29 @@ public class Ex1 {
         }
 
         //Exiba a quantidade de cores que o arco-íris tem
-        System.out.println("Total de "+arcoiris.size()+" cores");
+        System.out.println("\nTotal de "+arcoiris.size()+" cores");
+
+
+        //Exiba as cores em ordem alfabética
         Set<String> arcoiris2 = new TreeSet<>(new ComparatorOrdemAlfabetica());
         arcoiris2.addAll(arcoiris);
+        System.out.println("\n--Cores em ordem alfabética--");
         for(String cor: arcoiris2){
             System.out.println(cor);
         }
 
-        //Exiba as cores em ordem alfabética
+        //Exiba as cores na ordem inversa da que foi informada
+        List<String> listaCores = new ArrayList<>(arcoiris);
+        System.out.println("\n--Cores na ordem inversa da que foi informada--");
+        Collections.reverse(listaCores);
+        for(String cor: listaCores){
+            System.out.println(cor);
+        }
+
+
+
+
+
 
 
 
@@ -35,5 +50,6 @@ public class Ex1 {
             return o1.compareToIgnoreCase(o2);
         }
     }
+
 
 }
