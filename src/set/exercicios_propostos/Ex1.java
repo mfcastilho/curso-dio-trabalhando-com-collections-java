cb91279917fd63e8f7749d8857f64a0138ee2550
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-09-26 10:46:31 -0300

    declarando as cores do set

--- src/set/exercicios_propostos/Ex1.java
+++ src/set/exercicios_propostos/Ex1.java
@@ -1,7 +1,17 @@
 package set.exercicios_propostos;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Ex1 {
     public static void main(String[] args) {
 
+        Set<String> arcoiris = new HashSet<>(Arrays.asList("Vermelho", "Laranja", "Amarelo", "Verde", "Azul", "Azul-Escuro", "Violeta"));
+
+        
+
     }
+
 }
