66f5732399a81c72fa67879e0c2634fb96234e7b
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-09-26 10:52:29 -0300

    Exibindo a quantidade de cores que o arco-íris tem

--- src/set/exercicios_propostos/Ex1.java
+++ src/set/exercicios_propostos/Ex1.java
@@ -9,9 +9,17 @@ public class Ex1 {
     public static void main(String[] args) {
 
         Set<String> arcoiris = new HashSet<>(Arrays.asList("Vermelho", "Laranja", "Amarelo", "Verde", "Azul", "Azul-Escuro", "Violeta"));
+
+        //Exiba todas as cores uma abaixo da outra
+        System.out.println("--Todas as cores do Arco-Íris--");
         for(String cor: arcoiris){
             System.out.println(cor);
         }
+
+        //Exiba a quantidade de cores que o arco-íris tem
+        System.out.println("Total de "+arcoiris.size()+" cores");
+
+        //Exiba as cores em ordem alfabética
 
 
     }
