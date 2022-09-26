706500f250702ea65ca072c5bc81700aef583402
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-09-26 15:16:26 -0300

    Exibindo ordem de inserção do conjunto

--- src/set/exercicios_propostos/exercicio2/Principal.java
+++ src/set/exercicios_propostos/exercicio2/Principal.java
@@ -1,15 +1,24 @@
 package set.exercicios_propostos.exercicio2;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Principal {
     public static void main(String[] args) {
-        Set<LinguagemFavorita> listaLinguagensFavoritas = new HashSet<>(){{
+        //Ordem de Inserção
+        System.out.println("--\tOrdem de Inserção\t--");
+        Set<LinguagemFavorita> listaLinguagensFavoritas = new LinkedHashSet<>(){{
             add(new LinguagemFavorita("java", 1991, "intellij"));
             add(new LinguagemFavorita("javascript", 1995, "vscode"));
             add(new LinguagemFavorita("python", 1989, "pycharme"));
         }};
+        for(LinguagemFavorita linguagem: listaLinguagensFavoritas){
+            System.out.println(linguagem);
+        }
+
+        //Ordem Natural(nome)
+        System.out.println("--\tOrdem Natural(nome)\t--");
         
     }
 }
